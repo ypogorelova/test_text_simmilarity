41985466cfe802fc351db2d0a01064421a1bdfdc
ypogorelova <pogorelovayulia@gmail.com>
2022-02-16 19:14:11 +0200

    Delete matrix.py

--- matrix.py
+++
@@ -1,8 +0,0 @@
-rows = int(input("Enter the number of rows:"))
-columns = int(input("Enter the number of columns:"))
-
-print("Indexes of matrix:")
-for i in range(rows):
-    for j in range(columns):
-        print(i, j, sep=",", end=" ")
-    print()
